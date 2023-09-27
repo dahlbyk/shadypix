704aecfb67d345d8705e5f548d0fb9ab7793ab61
Keith Dahlby <dahlbyk@gmail.com>
2023-09-27 14:32:20 -0500

    Albums in drawer

--- src/app/(photos)/layout.tsx
+++ src/app/(photos)/layout.tsx
@@ -15,19 +15,29 @@ export default async function PhotosLayout({
 
   return (
     <>
-      <aside>
-        <h2>Albums</h2>
-        <ul>
+      <aside className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Albums</label>
+          {children}
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <div className="p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <ul className="menu">
+              <li><Link href="/">All Photos</Link></li>
+            </ul>
+            <h2 className='text-base-content'>Albums</h2>
+            <ul className="menu">
           {albums.map(a => (
             <li key={a.id}>
               <Link href={`/albums/${a.id}`}>{a.title}</Link>
             </li>
           ))}
-        </ul>
+            </ul>
+          </div>
+        </div>
       </aside>
-      <main>
-        {children}
-      </main>
     </>
   )
 }
